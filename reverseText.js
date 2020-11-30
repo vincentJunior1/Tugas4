c7c37a3e9b595002d2359bafc5984a9f69d603e8
vincentjuniorrr <vincent_cars@yahoo.com>
2020-11-30 11:18:48 +0700

    Finish

--- reverseText.js
+++ reverseText.js
@@ -1,15 +1,19 @@
 const reverseText = (teks) => {
-    let teksSplit = teks.split(' ');
-    let teksLength = teksSplit.length - 1;
-    let hasil = '';
-    for(let i = teksLength; i >= 0; i--){
-        if( i == 0){
-            hasil += teksSplit[i]
-        }else{
-            hasil += teksSplit[i] + ' '
+    if(typeof teks === 'string'){
+        let teksSplit = teks.split(' ');
+        let teksLength = teksSplit.length - 1;
+        let hasil = '';
+        for(let i = teksLength; i >= 0; i--){
+            if( i == 0){
+                hasil += teksSplit[i]
+            }else{
+                hasil += teksSplit[i] + ' '
+            }
         }
+        console.log(hasil)
+    }else{
+        console.log('input harus number')
     }
-    console.log(hasil)
 }
 
 
